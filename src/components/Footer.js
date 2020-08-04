a574648d4d9e0d78d7f024843188ec9e5af23d75
Rendy Dinar <r3ndydinar@gmail.com>
2020-08-04 15:16:03 +0700

    Initial Commit, Versi Beta Karena About Belum Lengkap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
 	  			<div className="row">
 	  				<div className="col s12 l6">
 	  					<h5>Tentang Kami</h5>
-	  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis impedit minus magni provident reiciendis dolorum ea. Itaque magnam, commodi enim.</p>
-	  					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis impedit minus magni provident reiciendis dolorum ea. Itaque magnam, commodi enim.</p>
+						<p>Pangkas Rambu Kambera, tempat pangkas rambut yang ada di wangga. Memastikan setiap pelanggan yang hendak ingin gunting rambut pulang akan dijamin ganteng.</p>
+						<p>Pangkas Rambu untuk orang dewasa/anak-anak pulang dijamin ganteng, Harga sangat terjangkau, hanya Rp.15.000 berlaku untuk semua orang</p>
 	  				</div>
 	  				<div className="col s12 l4 offset-l2">
 	  					<h5>Ikuti Kami</h5>
@@ -27,3 +27,4 @@ const Footer = () => {
 }
 
 export default Footer;
+
